fac91bcfcee9535d3aa8a389dbb0639736d78c35
gacjie <90205428+gacjie@users.noreply.github.com>
2023-11-08 12:30:27 +0800

    Add files via upload

---
+++ config/site.php
@@ -0,0 +1,38 @@
+<?php
+
+return array (
+  'name' => '自签证书管理器',
+  'beian' => '',
+  'cdnurl' => '',
+  'version' => '1.0.1',
+  'hash' => '',
+  'timezone' => 'Asia/Shanghai',
+  'forbiddenip' => '',
+  'languages' => 
+  array (
+    'backend' => 'zh-cn',
+    'frontend' => 'zh-cn',
+  ),
+  'fixedpage' => 'dashboard',
+  'categorytype' => 
+  array (
+    'default' => 'Default',
+    'page' => 'Page',
+    'article' => 'Article',
+    'test' => 'Test',
+  ),
+  'configgroup' => 
+  array (
+    'basic' => '基本配置',
+    'certificate' => '证书配置',
+  ),
+  'digest_alg' => 'sha256',
+  'private_key_bits' => '2048',
+  'private_key_type' => 'OPENSSL_KEYTYPE_RSA',
+  'randstr' => 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
+  'serialbit' => '1',
+  'document' => '/document',
+  'crt' => 'http://www.btpanel.cm/ssl/BTPanelCMInnRootCA.crt',
+  'crl' => 'http://www.btpanel.cm/ssl/BTPanelCMInnRootCA.crl',
+  'CPS' => 'http://www.btpanel.cm/BTPanelCMInnRootCACPS',
+);
